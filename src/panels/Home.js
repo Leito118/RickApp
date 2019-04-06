bffb0a9b6e4ab13a68e8f2782eb1c36ab5beb863
Leito118 <ileito118@gmail.com>
2019-04-07 00:40:49 +0300

    Изменён файл Home.js и  package.js

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -15,10 +15,10 @@ const Home = ({ id, go, fetchedUser }) => (
 			</ListItem>
 		</Group>}
 
-		<Group title="Navigation Example">
+		<Group title="Пример навигации">
 			<Div>
 				<Button size="xl" level="2" onClick={go} data-to="persik">
-					Show me the Persik, please
+					Персик
 				</Button>
 			</Div>
 		</Group>
