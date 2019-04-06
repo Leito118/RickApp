9d31b879e7113753833bef56b893ae4ef0a0612c
Leito118 <ileito118@gmail.com>
2019-04-07 00:26:57 +0300

    Updates

---
+++ precache-manifest.c2f8c0271581cab9ced8403e1df533df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c708be42412723ed9f5",
+    "url": "/RickApp/static/css/main.e2ce6f30.chunk.css"
+  },
+  {
+    "revision": "9c708be42412723ed9f5",
+    "url": "/RickApp/static/js/main.c5221ffe.chunk.js"
+  },
+  {
+    "revision": "62b41b906182eac0ad0f",
+    "url": "/RickApp/static/js/runtime~main.42958ffc.js"
+  },
+  {
+    "revision": "d495c259c17cfc6dd157",
+    "url": "/RickApp/static/css/2.afed59a8.chunk.css"
+  },
+  {
+    "revision": "d495c259c17cfc6dd157",
+    "url": "/RickApp/static/js/2.8b4b34f6.chunk.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/RickApp/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "012327acc10da374c3d438f48d299292",
+    "url": "/RickApp/index.html"
+  }
+];
