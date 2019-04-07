c62f8de163713bd24911fa3bd8719e800ccf9645
Leito118 <ileito118@gmail.com>
2019-04-07 15:13:22 +0300

    Изменения затронули: README.md, src/index.js, src/panels/Home.js

--- src/index.js
+++ src/index.js
@@ -9,6 +9,9 @@ import App from './App';
 // Init VK App
 connect.send('VKWebAppInit', {});
 
+//Init VK App Friends
+connect.send("VKWebAppGetFriends", {});
+
 // Если вы хотите, чтобы ваше веб-приложение работало в оффлайне и загружалось быстрее,
 // расскомментируйте строку с registerServiceWorker();
 // Но не забывайте, что на данный момент у технологии есть достаточно подводных камней
